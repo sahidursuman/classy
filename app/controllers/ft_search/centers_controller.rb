94ebc558c71ce7e4148ef80efadd442b4f4f1350
lucnv <nguyenluc312@gmail.com>
2017-12-19 21:51:26 +0700

    center-search-by-name

--- app/controllers/ft_search/centers_controller.rb
+++ app/controllers/ft_search/centers_controller.rb
@@ -1,5 +1,6 @@
 class FtSearch::CentersController < ApplicationController
   def index
-    @centers = Center.active.full_text_search(params[:center_name]).decorate
+    @centers = Center.active.full_text_search(params[:center_name]).page(params[:page])
+      .per(5).decorate
   end
 end
