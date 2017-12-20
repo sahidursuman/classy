c406830c001f1d38bcfa8a11e2a985d807c823c1
nguyenluc312 <nguyenluc312@gmail.com>
2017-12-20 20:18:43 +0700

    fix-review-verfication-notift

--- app/jobs/notify_new_review_verification_job.rb
+++ app/jobs/notify_new_review_verification_job.rb
@@ -1,6 +1,6 @@
 class NotifyNewReviewVerificationJob < ApplicationJob
   def perform review
-    Notification::NotifyCenterManagersService.new(review.center, review.center, review,
-      :new_review_verification, review.user).perform
+    Notification::NotifyCenterManagersService.new(review.center, review,
+      :new_review_verification).perform
   end
 end
--- app/services/notification/notify_center_managers_service.rb
+++ app/services/notification/notify_center_managers_service.rb
@@ -1,19 +1,17 @@
 class Notification::NotifyCenterManagersService
   attr_reader :center, :notifiable, :action, :creater, :creatable
 
-  def initialize center, notifiable, creatable, action, creater
+  def initialize center, notifiable, action
     @center = center
     @notifiable = notifiable
-    @creatable = creatable
     @action = action
-    @creater = creater
   end
 
   def perform
     ActiveRecord::Base.transaction do
       center.managers.each do |manager|
-        notification = Notification.create! recipient: manager, notifiable: notifiable,
-          action: action, user: creater, creatable: creatable
+        notification = Notification.create! recipient: manager, action: action,
+          notifiable: notifiable
       end
     end
   end
