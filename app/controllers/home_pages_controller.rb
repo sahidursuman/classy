8ee64bbfc0b3bf48fac618b37ee544592f67578a
oDoHongSon <hongsondhs@gmail.com>
2017-12-20 21:27:47 +0700

    add index-page, business page

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,4 +1,9 @@
 class HomePagesController < ApplicationController
+  layout "full_page"
+
+  def show
+    render template: "home_pages/#{params[:page]}"
+  end
   def index
     @current_user = current_user.try :decorate
   end
